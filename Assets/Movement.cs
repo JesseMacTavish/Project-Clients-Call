4696d6cdfff30078eee1311c8f8d82e2c8e7e80c
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-09-04 13:19:42 +0200

    A new beginning

---
+++ Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Transform trans;
+
+    // Use this for initialization
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.position = new Vector2(trans.position.x - 0.1f, 0);
+    }
+}
